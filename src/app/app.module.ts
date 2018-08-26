d403993a25b0bd54f997b06a25cc6f089cf46ee4
aaybhangu <Bhangu@Bhangu-2.local>
2018-08-26 18:13:48 -0400

    responsive fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
-import { SliderComponent } from './slider/slider.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { BodyComponent } from "./body/body.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SliderComponent } from "./slider/slider.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent,
     BodyComponent,
     FooterComponent,
     SliderComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
